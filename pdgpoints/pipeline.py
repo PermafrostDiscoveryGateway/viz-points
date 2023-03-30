9bb3f218342389d0708bb0ff8f1038ac77e218cb
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-03-30 15:58:18 -0700

    adding structure to process and utils

--- pdgpoints/pipeline.py
+++ pdgpoints/pipeline.py
@@ -1,11 +1,55 @@
 from pathlib import Path
 import subprocess
-from datetime import datetime, timedelta
+import getopt
+import os
 
-from py3dtiles.tileset.utils import TileContentReader
+from . import L
+from . import utils
 
-
-def run():
+def process(f, merge: bool=True):
     '''
+    Process the input file.
+
+    Variables:
+    :param f: The file to process
+    :type f: str or pathlib.Path
+    :param bool merge: Whether or not to merge with existing datasets in the output location (default: True)
+    :return: Processed data location
+    :rtype: str
+    '''
+    f = Path(f)
+    base_dir = os.path.split(f)
+    vlrcorrect_dir = os.path.join(base_dir, 'vlrcorrect')
+    archive_dir = os.path.join(base_dir, 'archive')
+    out_dir = os.path.join(base_dir, '3dtiles')
+
+    for d in [vlrcorrect_dir, archive_dir, out_dir]:
+        utils.make_dirs(d)
+
+    if merge:
+        utils.merge(in_dir=vlrcorrect_dir, out_dir=out_dir)
+    return str(dataloc)
+
+def cli():
+    '''
+    Parse the command options and arguments.
+    '''
+
+    try:
+        opts = getopt.getopt(sys.argv[1:], 'hf:',
+            ['help', 'file=',]
+            )[0]
+    except Exception as e:
+        L.error('%s: %s' % (repr(e), e))
     
-    '''
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            print(help_txt)
+            exit(0)
+        if o in ('-f', '--file'):
+            if os.path.exists(a):
+                f = a
+            else:
+                L.error('No file at %s' % (a))
+                exit(1)
+    process(f)
---
+++ pdgpoints/utils.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+from pathlib import Path
+from datetime import datetime
+
+from py3dtiles.tileset.utils import TileContentReader
+from . import L
+
+def make_dirs(d, exist_ok=True):
+    '''
+    Simple wrapper to create directory using os.makedirs().
+    Included is a logging command.
+
+    Variables:
+    :param pathlib.Path d: The directory to create
+    :param bool exist_ok: Whether to gracefully accept an existing directory (default: True)
+    '''
+    L.info('Creating dir %s' % (d))
+    os.makedirs(d, exist_ok=exist_ok)
+
+
+def merge(in_dir: Path, out_dir: Path):
+    '''
+    Merge the output 3dtiles files into a single dataset.
+
+    Variables:
+    :param pathlib.Path in_dir: The directory in which to look for files
+    :param pathlib.Path out_dir: The directory in which to place output
+    '''
+    L.info('Starting merge process in %s (step 3 of 3)' % (out_dir))
+    mergestart = datetime.now()
+    subprocess.run([
+        'py3dtiles',
+        'merge',
+        '--overwrite',
+        '--verbose',
+        out_dir
+    ])
+    mergetime = (datetime.now() - mergestart).seconds/60
+    L.info('Finished merge (%.1f min)' % (mergetime))
