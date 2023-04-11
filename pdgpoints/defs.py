a3d2b5d4ee78bd3508b84af249b669753a35133f
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-04-11 11:11:29 -0700

    adding help text

--- pdgpoints/defs.py
+++ pdgpoints/defs.py
@@ -1,7 +1,27 @@
 import os
+from ._version import __version__
+from datetime import datetime
 
+Y = datetime.now().year
 HELP_TXT = '''
-'''
+~~ pdgpoints version %s ~~
+ Ian Nesbitt / NCEAS %s
+Usage:
+tilepoints [ OPTIONS ] -f /path/to/file.las
+where OPTIONS := {
+    -h | --help
+            display this help message
+    -v | --verbose
+            display more informational messages
+    -c | --copy_I_to_RGB
+            copy intensity values to RGB channels
+    -m | --merge
+            merge all tilesets in the output folder (./3dtiles)
+    -a | --archive
+            copy original LAS files to a ./archive folder
+    -f | --file=/path/to/file.las
+            specify the path to a LAS or LAZ point cloud file
+''' % (__version__, Y)
 
 MOD_LOC = os.path.dirname(os.path.abspath(__file__))
 BIN_LOC = os.path.join(MOD_LOC, 'bin')
