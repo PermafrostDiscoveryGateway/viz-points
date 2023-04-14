484444fa1ce2617bd2b7c8729cfba7daa522608a
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-04-14 08:37:36 -0700

    addressing #5

--- pdgpoints/lastools_iface.py
+++ pdgpoints/lastools_iface.py
@@ -15,7 +15,27 @@ def log_subprocess_output(pipe, verbose=False):
     except CalledProcessError as e:
         L.error("Subprocess Error> %s: %s" % (repr(e), str(e)))
 
-def lasinfo(f, verbose=False):
+def run_proc(command, get_wkt: bool=False, verbose: bool=False):
+    '''
+    '''
+    L.debug('Command args: %s' % (command))
+    process = Popen(command,
+                    stdout=PIPE,
+                    stderr=STDOUT)
+    if get_wkt:
+        wktstr = check_output(('grep', 'EPSG'), stdin=process.stdout).decode().strip().strip('\n')
+    # pass pipe to be parsed
+    with process.stdout:
+        log_subprocess_output(process.stdout, verbose=verbose)
+    # start subprocess
+    exitcode = process.wait()
+    if exitcode != 0:
+        L.error('las2las rewrite subprocess exited with nonzero exit code--check log output')
+        exit(1)
+    if get_wkt:
+        return wktstr
+
+def lasinfo(f, verbose: bool=False):
     '''
     Use lasinfo to extract CRS info (in EPSG format) from a LAS or LAZ point cloud file.
 
@@ -30,20 +50,8 @@ def lasinfo(f, verbose=False):
         LASINFO_LOC,
         '-i', f,
         '-stdout',
-        #'-target_epsg', out_crs,
     ]
-
-    process = Popen(command,
-                    stdout=PIPE,
-                    stderr=STDOUT)
-
-    wkt = check_output(('grep', 'AUTHORITY'), stdin=process.stdout).decode().strip().strip('\n')
-    with process.stdout:
-        log_subprocess_output(process.stdout, verbose=verbose)
-    exitcode = process.wait()
-    if exitcode != 0:
-        L.error('lasinfo subprocess exited with nonzero exit code--check log output')
-        exit(1)
+    wkt = run_proc(command=command, get_wkt=True, verbose=verbose)
     L.debug('WKT string: %s' % (wkt))
     wktf = str(f) + '-wkt.txt'
     L.info('Writing WKT to %s' % (wktf))
@@ -82,39 +90,46 @@ def las2las(f,
     :param bool verbose: Whether or not to write STDOUT (output will always be written to log file)
     '''
     las2lasstart = datetime.now()
-    L.info('Using las2las to rewrite malformed VLR (e.g. from QT Modeler)... (step 1 of 3)')
     # construct command
     orig_output = output_file
     wktf = str(f) + '-wkt.txt'
-    command = [
-        LAS2LAS_LOC,
-        '-i', f,
-        '-set_ogc_wkt', '0',
-        '-load_ogc_wkt', wktf,
-    ]
+
     if intensity_to_RGB:
-        # add args to copy I into attrib 0
-        command.append('-copy_intensity_into_register')
-        command.append('0')
-        # if we're doing two operations, we need two filenames
-        ofn, ofe = os.path.splitext(output_file)
-        output_file = str(ofn) + 'i' + str(ofe)
-    # add args defining output file location
-    command.append('-o')
-    command.append(output_file)
-    L.debug('Command args: %s' % (command))
-    # construct subprocess
-    process = Popen(command,
-                    stdout=PIPE,
-                    stderr=STDOUT)
-    # pass pipe to be parsed
-    with process.stdout:
-        log_subprocess_output(process.stdout, verbose=verbose)
-    # start subprocess
-    exitcode = process.wait()
-    if exitcode != 0:
-        L.error('las2las rewrite subprocess exited with nonzero exit code--check log output')
-        exit(1)
+        L.info('Copying intensity to RGB by exploding and reforming LAS fields')
+        read_command = [
+            LAS2LAS_LOC,
+            '-i', f,
+            '-otxt',
+            '-oparse', 'xyziiiitanr',
+            '-stdout'
+        ]
+        write_command = [
+            LAS2LAS_LOC,
+            '-stdin',
+            '-itxt',
+            '-iparse', 'xyziRGBtanr',
+            '-scale_rgb_up',
+            '-load_ogc_wkt', wktf,
+            '-o', output_file
+        ]
+        L.debug('Cmd L of pipe: %s' % read_command)
+        L.debug('Cmd R of pipe: %s' % write_command)
+        r_process = Popen(read_command, stdout=PIPE)
+        w_process = Popen(write_command, stdin=r_process.stdout, stdout=PIPE)
+        r_process.stdout.close()
+        output = w_process.communicate()[0]
+        L.debug('Piped cmd output: %s' % output)
+    else:
+        L.info('Rewriting LAS to avoid VLR size errors (e.g. PDAL reading QTModeler files)')
+        command = [
+            LAS2LAS_LOC,
+            '-i', f,
+            '-set_ogc_wkt', '0',
+            '-load_ogc_wkt', wktf,
+            '-o', output_file
+        ]
+        run_proc(command=command, verbose=verbose)
+
     if archive:
         # move the file to the archive
         try:
@@ -127,45 +142,6 @@ def las2las(f,
             L.error('Archiving is on but no archive directory set! Cannot archive files!')
         except Exception as e:
             L.error('%s: %s' % (repr(e), e))
-    if intensity_to_RGB:
-        command = [
-            LAS2LAS_LOC,
-            '-i', output_file,
-            '-copy_register_into_R', '0',
-            '-copy_register_into_G', '0',
-            '-copy_register_into_B', '0',
-            '-set_register', '0', '0',
-            '-scale_rgb_up',
-            '-o', orig_output
-        ]
-        L.info('Copying intensity to register...')
-        L.debug('Command args: %s' % (command))
-        # new subprocess
-        process = Popen(command,
-                        stdout=PIPE,
-                        stderr=STDOUT)
-        # pass pipe to be parsed
-        with process.stdout:
-            log_subprocess_output(process.stdout, verbose=verbose)
-        # start subprocess
-        exitcode = process.wait()
-        if exitcode != 0:
-            L.error('las2las attribute copy subprocess exited with nonzero exit code--check log output')
-            exit(1)
-        # clean up
-        L.info('Cleaning up intermediate file %s' % output_file)
-        os.remove(output_file)
-    if archive:
-        archive_fn = os.path.join(os.getcwd(), archive_dir, os.path.split(f)[1])
-        f = pathlib.Path(os.path.join(os.getcwd(), f))
-        L.info('Archiving input file to %s' % (archive_fn))
-        try:
-            f.rename(archive_fn)
-        except FileNotFoundError as e:
-            # this happens for some reason
-            # even when the process is successful
-            # ??? maybe a datateam fs problem ?
-            pass
 
     las2lastime = (datetime.now() - las2lasstart).seconds
     L.info('Finished las2las (%s sec / %.1f min)' % (las2lastime, las2lastime/60))
