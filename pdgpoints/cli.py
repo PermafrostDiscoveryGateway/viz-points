afaa085f211f6c761b15e0cb4cf783bbeb1ba87a
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-06-07 13:19:26 -0700

    removing getopt, adding argparse (#19)

--- pdgpoints/cli.py
+++ pdgpoints/cli.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-import sys
-import getopt
+import argparse
 
 from . import L
 from . import defs
@@ -11,35 +10,26 @@ def cli():
     Parse the command options and arguments.
     '''
 
-    verbose, i_to_rgb, merge, archive, rgb_scale, translate_z = False, False, False, False, False, False
-    try:
-        opts = getopt.getopt(sys.argv[1:], 'hvcmas:z:f:',
-            ['help', 'verbose', 'copy_I_to_RGB', 'merge', 'archive', 'rgb_scale=', 'translate_z=', 'file=']
-            )[0]
-    except Exception as e:
-        L.error('%s: %s' % (repr(e), e))
-    
-    for o, a in opts:
-        if o in ('-h', '--help'):
-            print(defs.HELP_TXT)
-            exit(0)
-        verbose = True if o in ('-v', '--verbose') else verbose
-        i_to_rgb = True if o in ('-c', '--copy_I_to_RGB') else i_to_rgb
-        rgb_scale = a if o in ('-s', '--rgb_scale') else rgb_scale
-        translate_z = a if o in ('-z', '--translate_z') else translate_z
-        merge = True if o in ('-m', '--merge') else merge
-        archive = True if o in ('-a', '--archive') else archive
-        if o in ('-f', '--file'):
-            p = Path(a)
-            if not p.is_file():
-                L.error('No file at %s' % (a))
-                exit(1)
+    parser = argparse.ArgumentParser(description='Process some files.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Whether to log more messages')
+    parser.add_argument('-c', '--copy_I_to_RGB', action='store_true', help='Whether to copy intensity values to RGB')
+    parser.add_argument('-m', '--merge', action='store_true', help='Whether to use merge function')
+    parser.add_argument('-a', '--archive', action='store_true', help='Whether to archive the input dataset')
+    parser.add_argument('-s', '--rgb_scale', type=float, default=1.0, help='Scale multiplier for RGB values')
+    parser.add_argument('-z', '--translate_z', type=float, default=0.0, help='Float translation for z values')
+    parser.add_argument('-f', '--file', type=str, required=True, help='The file to process')
 
-    p = Pipeline(f=p,
-                 intensity_to_RGB=i_to_rgb,
-                 merge=merge,
-                 archive=archive,
-                 rgb_scale=rgb_scale,
-                 translate_z=translate_z,
-                 verbose=verbose)
+    args = parser.parse_args()
+    p = Path(args.file)
+    if not p.is_file():
+        L.error('No file at %s' % (p))
+        exit(1)
+
+    p = Pipeline(f=args.file,
+                 intensity_to_RGB=args.copy_i_to_rgb,
+                 merge=args.merge,
+                 archive=args.archive,
+                 rgb_scale=args.rgb_scale,
+                 translate_z=args.translate_z,
+                 verbose=args.verbose)
     p.run()
