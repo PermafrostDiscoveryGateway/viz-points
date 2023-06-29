755087681907ab3eb1648d53129ede91c700cb72
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-06-29 15:29:54 -0700

    updating definitions to reference pyegt (#14)

--- pdgpoints/cli.py
+++ pdgpoints/cli.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import argparse
+from pyegt.defs import MODEL_LIST, REGIONS
 
 from . import L
-from . import defs
 from .pipeline import Pipeline
 
 def cli():
@@ -16,8 +16,8 @@ def cli():
     parser.add_argument('-a', '--archive', action='store_true', help='Whether to archive the input dataset')
     parser.add_argument('-s', '--rgb_scale', type=float, default=1.0, help='Scale multiplier for RGB values')
     parser.add_argument('-z', '--translate_z', type=float, default=0.0, help='Float translation for z values')
-    parser.add_argument('-g', '--from_geoid', choices=defs.MODEL_LIST, default=None, help='The geoid, tidal, or geopotential model to translate from')
-    parser.add_argument('-r', '--geoid_region', choices=defs.REGIONS, default=defs.REGIONS[0], help='The NGS region (https://vdatum.noaa.gov/docs/services.html#step140)')
+    parser.add_argument('-g', '--from_geoid', choices=MODEL_LIST, default=None, help='The geoid, tidal, or geopotential model to translate from')
+    parser.add_argument('-r', '--geoid_region', choices=REGIONS, default=REGIONS[0], help='The NGS region (https://vdatum.noaa.gov/docs/services.html#step140)')
     parser.add_argument('-f', '--file', type=str, required=True, help='The file to process')
 
     args = parser.parse_args()
